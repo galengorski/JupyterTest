5a49c76c2e25ad3bdcd41a56cd163112c5043f1f
galengorski <32860978+galengorski@users.noreply.github.com>
2018-11-01 14:58:41 -0700

    Add files via upload

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
-install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','tidyverse','caret','leaps','MASS','neuralnet'))
 devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec()
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest','tidyverse','caret','leaps','MASS','neuralnet'))
+
