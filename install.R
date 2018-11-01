a4ac7d9f39e5022a7e36410dcd870a507ef6cd97
galengorski <32860978+galengorski@users.noreply.github.com>
2018-11-01 15:26:34 -0700

    Add files via upload

--- install.R
+++ install.R
@@ -1,3 +1,16 @@
-install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
+install.packages('repr')
+install.packages('IRdisplay')
+install.packages('evaluate')
+install.packages('crayon')
+install.packages('pbdZMQ')
+install.packages('devtools')
+install.packages('uuid')
+install.packages('digest')
+install.packages('tidyverse')
+install.packages('caret')
+install.packages('leaps')
+install.packages('MASS')
+install.packages('neuralnet')
+
 
 
