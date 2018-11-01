3c30090b1bd60a5a9612118e264d9b77b9d06788
galengorski <32860978+galengorski@users.noreply.github.com>
2018-11-01 15:32:46 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,11 +1,6 @@
-install.packages('repr')
-install.packages('IRdisplay')
-install.packages('evaluate')
-install.packages('crayon')
-install.packages('pbdZMQ')
+
 install.packages('devtools')
-install.packages('uuid')
-install.packages('digest')
+install.packages('rgdal')
 install.packages('caret')
 install.packages('leaps')
 install.packages('MASS')
